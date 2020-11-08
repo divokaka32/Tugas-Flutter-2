334ebd94ba819ebe94dd25b576ac36a041905b4c
divokaka32 <divo.kaka32@gmail.com>
2020-11-08 12:38:52 +0700

    Tugas Flutter 2

--- lib/Model/score_model.dart
+++ lib/Model/score_model.dart
@@ -1,26 +1,27 @@
 import 'package:scoped_model/scoped_model.dart';
+
 class ScoreModel extends Model {
   int _score = 0;
 
   int get score => _score;
 
   void increment3Score() {
-    _score += 3;
+    _score += 1;
     notifyListeners();
   }
 
   void increment2Score() {
-    _score += 2;
+    _score += 1;
     notifyListeners();
   }
 
   void decrement3Score() {
-    _score -= 3;
+    _score -= 1;
     notifyListeners();
   }
 
   void decrement2Score() {
-    _score -= 2;
+    _score -= 1;
     notifyListeners();
   }
 
--- lib/counter.dart
+++ lib/counter.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
 import 'package:team_score_counter/Model/score_model.dart';
+
 class Counter extends StatelessWidget {
   final String teamName;
 
@@ -9,40 +10,44 @@ class Counter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScopedModelDescendant<ScoreModel>(
-      builder: (context, child, model) => Container(
-        margin: EdgeInsets.only(top: 20, bottom: 20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              '$teamName:',
-              style: TextStyle(
-                fontSize: 40,
+      builder: (context, child, model) {
+        // ignore: deprecated_member_use
+        var display12 = Theme.of(context).textTheme.display2;
+        return Container(
+          margin: EdgeInsets.only(top: 20, bottom: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                '$teamName:',
+                style: TextStyle(
+                  fontSize: 40,
+                ),
               ),
-            ),
-            Text(
-              model.score.toString(),
-              style: Theme.of(context).textTheme.display1,
-            ),
-            Column(
-              children: <Widget>[
-                FlatButton(
-                  onPressed: model.increment3Score,
-                  child: Text('+ 3 POINTS'),
-                  color: Colors.lightBlueAccent,
-                  splashColor: Colors.green,
-                ),
-                FlatButton(
-                  onPressed: model.increment2Score,
-                  child: Text('+ 2 POINTS'),
-                  color: Colors.deepOrangeAccent,
-                  splashColor: Colors.redAccent,
-                ),
-              ],
-            )
-          ],
-        ),
-      ),
+              Text(
+                model.score.toString(),
+                style: display12,
+              ),
+              Column(
+                children: <Widget>[
+                  FlatButton(
+                    onPressed: model.increment3Score,
+                    child: Text('+ 1 POINTS'),
+                    color: Colors.lightBlueAccent,
+                    splashColor: Colors.green,
+                  ),
+                  FlatButton(
+                    onPressed: model.increment2Score,
+                    child: Text('+ 1 POINTS'),
+                    color: Colors.deepOrangeAccent,
+                    splashColor: Colors.redAccent,
+                  ),
+                ],
+              )
+            ],
+          ),
+        );
+      },
     );
   }
 }
